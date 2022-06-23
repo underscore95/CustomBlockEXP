0a7e5d255fc2e69f996993887c1684ef38212652
unfear-underscore <107675637+unfear-underscore@users.noreply.github.com>
2022-06-23 12:59:24 +0100

    cbereload command
    Reloads config

--- src/main/java/me/unfear/CustomBlockEXP/CustomBlockEXP.java
+++ src/main/java/me/unfear/CustomBlockEXP/CustomBlockEXP.java
@@ -2,23 +2,38 @@ package me.unfear.CustomBlockEXP;
 
 import java.util.HashMap;
 
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CustomBlockEXP extends JavaPlugin implements Listener {
+public class CustomBlockEXP extends JavaPlugin implements Listener, CommandExecutor {
 
 	private boolean preventSilk = true;
-	private HashMap<Material, Integer> expAmounts = new HashMap<>();
+	private HashMap<Material, Integer> expAmounts;
 	
 	public void onEnable() {
 		saveDefaultConfig();
 		
+		updateConfig();
+		
+		getCommand("cbereload").setExecutor(this);
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	public void updateConfig() {
+		reloadConfig();
 		preventSilk = getConfig().getBoolean("prevent-silk");
+		expAmounts = new HashMap<>();
+		
 		for (String key : getConfig().getConfigurationSection("blocks").getKeys(false)) {
 			try {
 				Material mat = Material.valueOf(key);
@@ -29,8 +44,6 @@ public class CustomBlockEXP extends JavaPlugin implements Listener {
 				getLogger().warning(key + " is not a valid block type! It will drop the default experience.");
 			}
 		}
-		
-		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@EventHandler
@@ -38,5 +51,11 @@ public class CustomBlockEXP extends JavaPlugin implements Listener {
 		if (!expAmounts.containsKey(e.getBlock().getType()) || e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
 		if (preventSilk && e.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) return;
 		e.setExpToDrop(expAmounts.get(e.getBlock().getType()));
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		updateConfig();
+		sender.sendMessage(ChatColor.GREEN + "CustomBlockEXP has been reloaded!");
+		return true;
 	}
 }
